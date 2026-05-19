7f77a346fa56dc21a2e64b2f43f5f9725d09a85f
Roll <setokazuma0617@gmail.com>
2026-05-19 12:19:17 +0900

    長押しと、発射方向を指定するShootSampleを追加

---
+++ 2DSample/06-2-Shoot-Hold/SourceCode/main.cpp
@@ -0,0 +1,16 @@
+﻿/*
+「Sample-06　弾を撃つ」
+
+内容     : 弾を撃つ処理を作成します！
+ポイント : std::vectorで弾を管理し、大量の弾を扱いやすくします！
+*/
+
+#include <DxLib.h>
+#include "game.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	auto game = Game();
+
+	return game.Run();
+}
---
+++ 2DSample/06-3-Shoot-ShootDir/SourceCode/window.h
@@ -0,0 +1,7 @@
+﻿#include <math.hpp>
+
+namespace window
+{
+	constexpr Vector2 kSize = { 1920.0f, 1080.0f };
+	constexpr int kColorBitDepth = 32;
+}
